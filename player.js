2e7441d5660f91e3cd78b60ba3ce54bbdec81447
akshat <akshatrane2007@gmail.com>
2026-09-08 16:11:09 +0530

    Update CLI music player

--- player.js
+++ player.js
@@ -1,1 +1,14 @@
-const fs=require("fs")
+const fs = require("fs");
+const path = require("path");
+const readline = require("readline");
+const player = require("play-sound")();
+const musicfolder=path.join(__dirname,"songs")
+const songs = fs.readdirSync(musicfolder).filter(x=>x.endsWith(".mp3"))
+function show(){
+
+
+    songs.forEach((song, index) => {
+    console.log(`${index + 1}. ${song}`);
+  });
+
+}
